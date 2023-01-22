ddb786a251a1c2bdb46d8de69315d1da61123d2a
juliasavritskaya <mnkqn0211>
2023-01-22 15:47:45 +0400

    Initial commit

---
+++ src/main/java/com/mnkqn/rentee/RenteeApplication.java
@@ -0,0 +1,13 @@
+package com.mnkqn.rentee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnkqn/rentee/RenteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnkqn.rentee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
